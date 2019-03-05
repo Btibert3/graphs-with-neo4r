938784f47e07108a20c7e084db62a21209209980
Brock Tibert <btibert3@gmail.com>
2019-03-04 21:03:22 -0500

    update code for pulling data

--- main.R
+++ main.R
@@ -4,6 +4,8 @@ options(stringsAsFactors = FALSE)
 library(rtweet)
 library(neo4r)
 library(dplyr)
+library(stringr)
+library(tidyr)
 library(purrr)
 
 
@@ -23,10 +25,66 @@ token = create_token(
 ## get the data in a dataframe
 TAGS = "#rstats OR #r4ds OR #neo4j"
 rt = search_tweets(q = TAGS,
-                   n = 18000,
+                   n = 100,
                    include_rts = TRUE)
 dim(rt)
 glimpse(rt)
+saveRDS(rt, "data/tweets.rds")
+
+
+## learnings: even though text contains #rstats, hashtag didnt show in list
 
 
 ############### parse the data before the import into neo4j
+
+######## hashtags -- twitter api is missing hashtags, even though they are part of search
+## extract manually into a list column and make a long dataframe with tweet id and tags
+## https://stackoverflow.com/questions/13762868/how-do-i-extract-hashtags-from-tweets-in-r
+TAG_REGEX = "#\\S+"
+tweets = rt %>%  mutate(hashtags_clean = str_extract_all(text, TAG_REGEX))
+tweet_tags = tweets %>% 
+  select(status_id, hashtags_clean) %>% 
+  unnest(hashtags_clean)
+
+
+######## mentions 
+tweet_mentions = rt %>% 
+  select(status_id, 
+         mentions_user_id, 
+         mentions_screen_name) %>% 
+  unnest() %>% 
+  drop_na()
+
+
+######## tweet is retweet of user 
+tweet_retweet = rt %>% 
+  filter(is_retweet) %>% 
+  select(status_id, 
+         retweet_status_id, 
+         retweet_user_id, 
+         retweet_screen_name)
+
+
+######## tweet quotes another tweet
+tweet_quote = rt %>% 
+  filter(is_quote) %>% 
+  select(status_id, 
+         quoted_status_id, 
+         quoted_user_id, 
+         quoted_screen_name)
+
+
+
+######## user info
+user = rt %>% 
+  select(user_id, 
+         screen_name,
+         account_created_at,
+         statuses_count,
+         favourites_count,
+         followers_count,
+         followers_count)
+
+
+
+
